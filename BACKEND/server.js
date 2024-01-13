63ac3d981dec8a32e30e5cd9bf0fb36f2204c1f5
h2oalphaYT <it21802676@my.sliit.lk>
2024-01-13 16:33:27 +0530

    sdd

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,19 +1,27 @@
-const express = require("express")
+const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-
-
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+// const __dirname =path.resolve();
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.json());
+
+
+app.use(express.static(path.join(__dirname, 'frontend/UserManagement/dist')));
+
+app.get('*' , (req,res) => {
+  res.sendFile(path.join(__dirname ,'frontend', 'UserManagement' ,'dist' ,'index.html'))
+})
 
 const URL = process.env.MONGODB_URL;
 
@@ -33,18 +41,16 @@ app.use("/api/user", userRoutes);
 const authRoutes = require("./routes/authRoute");
 app.use("/api/auth", authRoutes);
 
-app.use((err,req,res,next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success:false,
-        message,
-        statusCode,
-    })
-})
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port number: ${PORT}`);
 });
-
